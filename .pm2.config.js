795650d489554ca84b9f48177f265bcf55afb60a
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-10-08 14:26:17 +0100

    chore(pm2): watch .env files for config changes; set instances to max

--- .pm2.config.js
+++ .pm2.config.js
@@ -1,16 +1,19 @@
-// Used by PM2 for deployment
+// PM2 ecosystem config
 module.exports = {
 	apps: [
 		{
 			cwd: __dirname,
 			env: {
+				NODE_ENV: 'development'
+			},
+			env_production: {
 				NODE_ENV: 'production'
 			},
 			exec_mode: 'cluster',
-			instances: 4,
+			instances: 'max',
 			name: 'sider-obfu',
 			script: './src/index.js',
-			watch: ['./src/config.js', '.env.production']
+			watch: ['.env.development', '.env.production']
 		}
 	]
 };
