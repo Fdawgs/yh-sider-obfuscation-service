ed5be5a09cb7edf78548fdebb18d5535920c1f2b
Frazer Smith <frazerry@outlook.com>
2020-12-30 12:34:13 +0000

    refactor(plugins/keycloak): change `option` param handling
    BREAKING CHANGE: keycloak plugin function now expects `options.keycloak` not `options`

---
+++ src/plugins/keycloak-access-token/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable jsdoc/require-param-description */
+const fastifyPlugin = require("fastify-plugin");
+const createError = require("http-errors");
+const request = require("axios");
+const queryString = require("querystring");
+
+/**
+ * @author Frazer Smith
+ * @description Pre-handler plugin that retrieves Keycloak access token for passed user.
+ * @param {Function} server - Fastify instance.
+ * @param {object} options
+ * @param {object} options.keycloak - Keycloak endpoint access options values.
+ * @param {boolean} options.keycloak.enabled - Toggle to enable or disable use of Keycloak.
+ * @param {object} options.keycloak.serviceAuthorisation
+ * @param {object} options.keycloak.serviceAuthorisation.form
+ * @param {string} options.keycloak.serviceAuthorisation.form.client_id
+ * @param {string} options.keycloak.serviceAuthorisation.form.client_secret
+ * @param {string} options.keycloak.serviceAuthorisation.form.grant_type
+ * @param {string} options.keycloak.serviceAuthorisation.form.password
+ * @param {string} options.keycloak.serviceAuthorisation.form.username
+ * @param {string} options.keycloak.serviceAuthorisation.url
+ * @param {object} options.keycloak.requestToken
+ * @param {object} options.keycloak.requestToken.form
+ * @param {string} options.keycloak.requestToken.form.audience
+ * @param {string} options.keycloak.requestToken.form.client_id
+ * @param {string} options.keycloak.requestToken.form.client_secret
+ * @param {string} options.keycloak.requestToken.form.grant_type
+ * @param {string} options.keycloak.requestToken.form.request_subject
+ * @param {string} options.keycloak.requestToken.form.request_token_type
+ * @param {string} options.keycloak.requestToken.url
+ */
+async function plugin(server, options) {
+	// Don't add preHandler hook and attempt to retrieve access tokens if Keycloak not enabled
+	if (options && options.keycloak && options.keycloak.enabled === true) {
+		server.addHook("preHandler", async (req, res) => {
+			try {
+				const { requestToken, serviceAuthorisation } = options.keycloak;
+
+				// Service authorisation to retrieve subject access token
+				const serviceAuthResponse = await request.post(
+					serviceAuthorisation.url,
+					queryString.stringify(serviceAuthorisation.form)
+				);
+
+				requestToken.form.subject_token =
+					serviceAuthResponse.data.access_token;
+
+				// Expects the practitioner query to be in [system]|[code] format
+				requestToken.form.requested_subject = req.query.practitioner.split(
+					"|"
+				)[1];
+
+				// Request access token for user
+				const userAccessResponse = await request.post(
+					requestToken.url,
+					queryString.stringify(requestToken.form)
+				);
+				req.query.access_token = userAccessResponse.data.access_token;
+				return;
+			} catch (err) {
+				server.log.error(err);
+				res.send(
+					createError(
+						500,
+						"Unable to retrieve Keycloak access token(s)"
+					)
+				);
+			}
+		});
+	}
+}
+
+module.exports = fastifyPlugin(plugin);
--- test_resources/mocks/keycloak-config.mock.js
+++ test_resources/mocks/keycloak-config.mock.js
@@ -3,27 +3,29 @@
  * https://github.com/keycloak/keycloak-documentation/blob/master/securing_apps/topics/token-exchange/token-exchange.adoc
  */
 const keycloakRetrieveConfig = {
-	enabled: "true",
-	requestToken: {
-		form: {
-			audience: "mock-audience",
-			client_id: "mock-id",
-			client_secret: "mock-secret",
-			grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
-			requested_token_type:
-				"urn:ietf:params:oauth:token-type:access_token",
+	keycloak: {
+		enabled: true,
+		requestToken: {
+			form: {
+				audience: "mock-audience",
+				client_id: "mock-id",
+				client_secret: "mock-secret",
+				grant_type: "urn:ietf:params:oauth:grant-type:token-exchange",
+				requested_token_type:
+					"urn:ietf:params:oauth:token-type:access_token",
+			},
+			url: "http://127.0.0.1:3000/token",
 		},
-		url: "http://127.0.0.1:3000/token",
-	},
-	serviceAuthorisation: {
-		form: {
-			client_id: "mock-id",
-			client_secret: "mock-secret",
-			grant_type: "password",
-			password: "mock-password",
-			username: "mock-user@ydh.nhs.uk",
+		serviceAuthorisation: {
+			form: {
+				client_id: "mock-id",
+				client_secret: "mock-secret",
+				grant_type: "password",
+				password: "mock-password",
+				username: "mock-user@ydh.nhs.uk",
+			},
+			url: "http://127.0.0.1:3000/token",
 		},
-		url: "http://127.0.0.1:3000/token",
 	},
 };
 
