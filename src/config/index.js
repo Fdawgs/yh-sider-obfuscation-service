9ae173cc37643059ffa397e120e7a8d37b69b56d
Frazer Smith <frazerry@outlook.com>
2021-05-11 13:34:51 +0100

    fix(config): `LOG_LEVEL` env variable validation

--- src/config/index.js
+++ src/config/index.js
@@ -84,8 +84,8 @@ async function getConfig() {
 			)
 			.prop(
 				"LOG_LEVEL",
-				S.string()
-					.enum([
+				S.anyOf([
+					S.string().enum([
 						"fatal",
 						"error",
 						"warn",
@@ -93,8 +93,9 @@ async function getConfig() {
 						"debug",
 						"trace",
 						"silent",
-					])
-					.default("info")
+					]),
+					S.null(),
+				]).default("info")
 			)
 			.prop("LOG_ROTATION_DATE_FORMAT", S.string().default("YYYY-MM-DD"))
 			.prop("LOG_ROTATION_FILENAME", S.anyOf([S.string(), S.null()]))
