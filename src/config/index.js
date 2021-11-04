bebc2f97035d98ef9ac4374e624e519a08bca09d
Frazer Smith <frazer.dev@outlook.com>
2021-11-04 14:57:32 +0000

    style(config): rename `fsp` variable to `fs`

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const Ajv = require("ajv");
 const addFormats = require("ajv-formats");
 const envSchema = require("env-schema");
 const S = require("fluent-json-schema");
-const fsp = require("fs").promises;
+const fs = require("fs").promises;
 const pino = require("pino");
 const rotatingLogStream = require("file-stream-rotator");
 const secJSON = require("secure-json-parse");
@@ -367,9 +367,9 @@ async function getConfig() {
 		try {
 			config.fastifyInit.https = {
 				// eslint-disable-next-line security/detect-non-literal-fs-filename
-				cert: await fsp.readFile(env.HTTPS_SSL_CERT_PATH),
+				cert: await fs.readFile(env.HTTPS_SSL_CERT_PATH),
 				// eslint-disable-next-line security/detect-non-literal-fs-filename
-				key: await fsp.readFile(env.HTTPS_SSL_KEY_PATH),
+				key: await fs.readFile(env.HTTPS_SSL_KEY_PATH),
 			};
 		} catch (err) {
 			throw Error(
@@ -383,7 +383,7 @@ async function getConfig() {
 			config.fastifyInit.https = {
 				passphrase: env.HTTPS_PFX_PASSPHRASE,
 				// eslint-disable-next-line security/detect-non-literal-fs-filename
-				pfx: await fsp.readFile(env.HTTPS_PFX_FILE_PATH),
+				pfx: await fs.readFile(env.HTTPS_PFX_FILE_PATH),
 			};
 		} catch (err) {
 			throw Error(
