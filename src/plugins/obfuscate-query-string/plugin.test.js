e5a15de1e0883c20b2b448c77d352c57c5caac29
Frazer Smith <frazer.dev@outlook.com>
2021-11-12 14:50:07 +0000

    refactor(plugins): remove redundant dependency (#541)

--- src/plugins/obfuscate-query-string/plugin.test.js
+++ src/plugins/obfuscate-query-string/plugin.test.js
@@ -1,7 +1,6 @@
 const cloneDeep = require("lodash").cloneDeep;
 const faker = require("faker/locale/en_GB");
 const Fastify = require("fastify");
-const sensible = require("fastify-sensible");
 const plugin = require(".");
 const getConfig = require("../../config");
 
@@ -27,7 +26,6 @@ describe("Query String Obfuscation plugin", () => {
 
 	beforeEach(() => {
 		server = Fastify();
-		server.register(sensible);
 
 		server.get("/", (req, res) => {
 			res.send(req.query);
