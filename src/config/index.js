49a22abcdcb673d7178ed7c7f3cb7caa04cac84f
Frazer Smith <frazer.dev@outlook.com>
2023-02-20 11:10:06 +0000

    fix(config): misleading thrown error message

--- src/config/index.js
+++ src/config/index.js
@@ -364,7 +364,7 @@ async function getConfig() {
 			};
 		} catch (err) {
 			throw new Error(
-				`No such file or directory ${err.path} for SSL cert/key, falling back to HTTP`
+				`No such file or directory ${err.path} for SSL cert/key`
 			);
 		}
 	}
@@ -380,7 +380,7 @@ async function getConfig() {
 			};
 		} catch (err) {
 			throw new Error(
-				`No such file or directory ${err.path} for PFX file, falling back to HTTP`
+				`No such file or directory ${err.path} for PFX file`
 			);
 		}
 	}
